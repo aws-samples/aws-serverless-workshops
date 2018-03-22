d567130c5c29db149b7be935f6276a996c6aa8a6
Tom McLaughlin <tom@serverlessops.io>
2018-03-22 10:31:36 -0400

    Center on Boston, the center of the universe.

--- NewDeveloper/1_DeveloperEnvironmentSetup/wild-rydes-website/static/js/esri-map.js
+++ NewDeveloper/1_DeveloperEnvironmentSetup/wild-rydes-website/static/js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [-122.31, 47.60],
+            center: [-71.05, 42.36],
             container: 'map',
             map: map,
             zoom: 12

48342da158ad47d7c3bbb92a941fe7615e75161c
juan4aws <jlamadri@amazon.com>
2018-09-28 15:29:02 +0000

    changes from cloud9

--- MultiRegion/2_UI/src/environments/environment.ts
+++ MultiRegion/2_UI/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: 'us-east-1:f0558b0f-c436-4ed1-830f-270d6dcfc8e1',
+  cognitoIdentityPoolId: 'us-east-1:54ad0168-9515-4040-93ea-8cb60a88378c',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
@@ -17,6 +17,6 @@ export const environment = {
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: 'https://7fepp2xfh3.execute-api.us-east-1.amazonaws.com/prod/'
+  ticketAPI: 'https://69dd7cot96.execute-api.us-east-1.amazonaws.com/prod/'
 
 };

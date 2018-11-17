5e5b4d517083999bdb1183bad5ab36d6f9bd0dfb
joetechem <joe@techemstudios.com>
2018-11-16 23:36:17 -0500

    removing extra apostrophe to save people time

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
     }
 };

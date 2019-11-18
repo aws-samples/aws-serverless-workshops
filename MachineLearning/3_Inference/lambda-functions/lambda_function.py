97dedb7713fc1619e946a096a323db7b185af11b
Justin McWhirter <justin.mcwhirter@gmail.com>
2019-11-18 17:06:24 -0600

    Checking in the exact file as zip so error line numbers match up.

--- MachineLearning/3_Inference/lambda-functions/lambda_function.py
+++ MachineLearning/3_Inference/lambda-functions/lambda_function.py
@@ -1,11 +1,5 @@
 from __future__ import print_function
 
-# Function Name:
-# Inference Function (function C)
-
-# Function Path:
-# MachineLearning/3_Inference/lambda-functions/lambda_function.py
-
 import mxnet as mx
 import json
 import boto3
@@ -17,7 +11,7 @@ def download_model():
     # UPDATE THIS PATH TO YOUR S3 KEY
     key = os.environ['MODEL_PATH']
     boto3.resource('s3').Bucket(bucket).download_file(key, '/tmp/model.tar.gz')
-    
+
     os.system('cd /tmp && tar -zxvf model.tar.gz')
     os.system('cd /tmp && unzip model_algo-1')
 
@@ -33,7 +27,7 @@ def make_prediction(input):
 
     # Next bind the module with the data shapes.
     mod.bind(data_shapes=data_iter.provide_data)
-    
+
     # Predict
     results = mod.predict(data_iter)
     return round(results.asnumpy().tolist()[0][0], 2)
@@ -51,8 +45,7 @@ mod._arg_params['fc0_weight'].asnumpy().flatten()
 mod._arg_params['fc0_bias'].asnumpy().flatten()
 
 def handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
+    print("Received event: " + json.dumps(event, indent=2))
     # make_prediction({ "distance": 100, "healthpoints": 10000, "magicpoints": 50, "TMAX": 1, "TMIN": 1, "PRCP": 240 })
     result = make_prediction(json.loads(event['body']))
     return { "statusCode": 200, "body": result }
-        

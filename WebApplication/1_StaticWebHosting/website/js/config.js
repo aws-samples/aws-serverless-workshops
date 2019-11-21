259b2024bc8cc3a4e96e9362b6eac2444134f259
Alena <46408871+Alka888@users.noreply.github.com>
2019-11-21 15:31:11 -0600

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_l4wlJoC0A', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4m3m815epghna4jiqdju8bnv3g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
